298730507f824f81239990e40888a3dd180fc07a
tzikoulisathan <tzikoulisathan@gmail.com>
2024-01-14 20:38:42 +0200

    Initial Commit

---
+++ AgroCoop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgroCoop
+//
+//  Created by Athanasios Tzikoulis on 14/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
