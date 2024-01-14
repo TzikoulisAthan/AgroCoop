dacf70b6f620fd27809820eefe77c00f452bfced
tzikoulisathan <tzikoulisathan@gmail.com>
2024-01-14 20:58:14 +0200

    Initial commit

---
+++ AgroCoop/Controllers/Core/ACTabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AgroCoop
+//
+//  Created by Athanasios Tzikoulis on 14/1/24.
+//
+
+import UIKit
+
+class ACTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        setupTabs()
+    }
+    
+    
+    private func setupTabs() {
+        
+    }
+
+
+}
+
