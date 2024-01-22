3a580d7542c9c54c71c2b20f4c920179bce64d62
tzikoulisathan <tzikoulisathan@gmail.com>
2024-01-22 17:23:38 +0200

    Get daily prices from Firebase functionality

---
+++ AgroCoop/Models/ProductPricesModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductPriceModel.swift
+//  AgroCoop
+//
+//  Created by Athanasios Tzikoulis on 16/1/24.
+//
+
+import Foundation
+
+struct ProductPricesModel {
+    var productName: String
+    var productPrice: String
+}
---
+++ AgroCoop/Views/ACContactViewControllerView.swift
@@ -0,0 +1,20 @@
+//
+//  ACContactViewControllerView.swift
+//  AgroCoop
+//
+//  Created by Athanasios Tzikoulis on 17/1/24.
+//
+
+import UIKit
+
+class ACContactViewControllerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
